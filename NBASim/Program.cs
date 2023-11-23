f95bf6a2bf120aba924b58c556532ca1aaf27e40
Mosh <ram487@cornell.edu>
2023-11-23 18:38:33 -0500

    setup repo in vs studio

---
+++ NBASim/Program.cs
@@ -0,0 +1,21 @@
+﻿
+var test = new Simulation(82, "Celtics");
+
+Console.WriteLine(test.getSeasonLength());
+
+public class Simulation
+{
+    private int numGames;
+    private String teamName;
+
+
+    public Simulation(int seasonLen, String team)
+    {
+        numGames = seasonLen;
+        teamName = team;
+    }
+    public int getSeasonLength()
+    {
+        return numGames;
+    }
+}
