967ee1a99949ec1e050b95648dd9197b65ede615
Mosh <ram487@cornell.edu>
2023-11-24 11:48:23 -0500

    fixed state running issue, added more trackers.

--- NBASim/Program.cs
+++ NBASim/Program.cs
@@ -1,21 +1,73 @@
-﻿
-var test = new Simulation(82, "Celtics");
-
-Console.WriteLine(test.getSeasonLength());
-
-public class Simulation
+﻿public class Simulation
 {
     private int numGames;
     private String teamName;
 
+    private int seasonsPlayed;
+    private int totalWins;
+    private int totalGames;
+    private int chipsWon;
+
+
 
     public Simulation(int seasonLen, String team)
     {
+        seasonsPlayed = 0;
+        totalGames = 0;
+        totalWins = 0;
+        chipsWon = 0;
+
         numGames = seasonLen;
         teamName = team;
+
     }
-    public int getSeasonLength()
+    public int GetSeasonLength()
     {
         return numGames;
+    }
+    public int GetSeasonsPlayed()
+    {
+        return seasonsPlayed;
+    }
+    public int GetTotalWins()
+    {
+        return totalWins;
+    }
+    public int GetTotalGames()
+    {
+        return totalGames;
+    }
+    public int GetChipsWon()
+    {
+        return chipsWon;
+    }
+
+
+    public int RunSeason()
+    {
+        seasonsPlayed++;
+        int g = 0;
+        int acc = 0;
+
+        Random r = new Random();
+
+        while (g < numGames)
+        {
+            var i = r.NextDouble();
+            if (i <= .5)
+            {
+                acc++;
+                totalWins++;
+            }
+            g++;
+        }
+
+        totalGames += g;
+        if (acc >= 48)
+        {
+            chipsWon++;
+        }
+
+        return acc;
     }
 }
--- NBASim/State.cs
+++ NBASim/State.cs
@@ -8,7 +8,16 @@ namespace NBASim
 		{
 			int state = 0;
 
-			while (state != 2)
+            Console.WriteLine("How many games would you like to play?");
+            String? games = Console.ReadLine();
+            Console.WriteLine($"Starting Simulation with '{games}' games.");
+
+            Console.WriteLine("What team do you choose?");
+            String? team = Console.ReadLine();
+
+            Simulation sim = new Simulation(82, "Celtics");
+
+            while (state != 3)
 			{
                 switch (state)
                 {
@@ -20,20 +29,23 @@ namespace NBASim
 
                     case <= 1:
                         Console.WriteLine("State = 1. Game in progress");
-                        Console.WriteLine("How many games would you like to play?");
-                        String? input = Console.ReadLine();
-                        Console.WriteLine($"Starting Simulation with '{input}' games.");
+                        
 
-                        Simulation sim = new Simulation(82, "Celtics");
-
-                        Console.WriteLine(sim.getSeasonLength());
+                        while (sim.GetSeasonsPlayed() < 20)
+                        {
+                            Console.WriteLine($"In season '{sim.GetSeasonsPlayed() + 1}', your '{team}' team won '{sim.RunSeason()}' games.");
+                        }
 
                         Console.WriteLine("Simulation Ended. Moving to state 2");
+                        state = 2;
                         break;
 
                     case <= 2:
-                        Console.WriteLine("State = 2. End.");
-                        state = 2;
+                        Console.WriteLine("State = 2. Final Stats:");
+                        Console.WriteLine($"Total Wins: '{sim.GetTotalWins()}'");
+                        Console.WriteLine($"Total Games Played: '{sim.GetTotalGames()}'");
+                        Console.WriteLine($"Total Championships Won: '{sim.GetChipsWon()}'");
+                        state = 3;
                         break;
 
                     default:
